603616508fbb37e91590cf4245008f461566db7f
Roman Dziuba <71341700+romadzy@users.noreply.github.com>
2020-09-28 19:39:26 +0300

    Add files via upload

---
+++ Сredit.php
@@ -0,0 +1,35 @@
+<?php
+
+function creditPayments($loanAmount, $percentage, $commission, $otherPayments, $monthlyPayments) {
+
+    $monthCount = 0;
+    $bankPayments = 0;
+
+    $percentage /= 100;
+    $loanAmount += $otherPayments;
+
+    for ($i = 0; $loanAmount > 0; $i++) {
+      if ($i == 0) {
+        $loanAmount -= $monthlyPayments;
+        $monthCount++;
+        continue;
+      }
+
+      $bankPayments += $loanAmount +($loanAmount * $percentage + $commission) - $loanAmount;
+      $loanAmount += $loanAmount * $percentage + $commission - $monthlyPayments;
+      $monthCount++;
+  }
+
+    echo "Bank Payments: $bankPayments UAH<br>";
+    echo "Time for loan repayment: $monthCount month/s";
+}
+
+echo "<h3>Homo Credit </h3>";
+creditPayments(34999, 4, 500, 0, 5000);
+
+echo "<h3>Softbank</h3>";
+creditPayments(34999, 3, 1000, 0,5000);
+
+echo "<h3>StrawberryBank</h3>";
+creditPayments(34999, 2, 0, 6666, 5000);
+?>
